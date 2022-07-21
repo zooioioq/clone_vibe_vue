e3044c0a8abaf439288dd2191cb160dc4c884018
ictdDev <zooi.oi.oq@gmail.com>
2022-07-21 15:58:21 +0900

    header 생성 및 nav list 데이터 바인딩

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,72 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- wrap -->
+  <div class="wrap">
+    <!-- header -->
+    <header class="header">
+      <!-- headerLogo -->
+      <h1 class="headerLogo"> NAVER VIBE </h1>
+      <!-- // headerLogo -->
+      <!-- loginBtn -->
+      <div class="loginBtn">
+        <p><a href="#"> 로그인 </a></p>
+      </div>
+      <!-- // loginBtn -->
+      <!-- searchBox -->
+      <div class="searchBox">
+        <form>
+          <input type="text" class="search" placeholder="VIBE 검색">
+        </form>
+      </div>
+      <!-- // searchBox -->
+      <!-- nav -->
+      <nav class="nav">
+        <ul>
+          <li v-for="(n,v) in navList" :key="v"><span><a href="#">{{ navList[v] }}</a></span></li>
+        </ul>
+      </nav>
+      <!-- // nav -->
+      <!-- subLink -->
+      <div class="subLink">
+        <ul>
+          <li v-for="(s,b) in subLink" :key="b"><a href="#"> {{ subLink[b] }} </a></li>
+        </ul>
+      </div>
+      <!-- // subLink -->
+      <!-- CSLink -->
+      <div class="CSLink">
+        <ul>
+          <li v-for="(c,s) in CSLink" :key="s"><a href="#"> {{ CSLink[s] }} </a></li>
+        </ul>
+      </div>
+      <!-- // CSLink -->
+      <!-- headerBtn -->
+      <div class="headerBtn">
+        <p class="highlight"><a href="#"> 멤버십 구독 </a></p>
+        <p><a href="#"> 데스크톱앱 설치 </a></p>
+      </div>
+      <!-- // headerBtn -->
+    </header>
+    <!-- // header -->
+  </div>
+  <!-- // wrap -->
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
+  data(){
+    return{ // 데이터 보관함
+      navList : ['투데이','차트','최신앨범','DJ 스테이션','VIBE MAG','이달의 노래'],
+      subLink : ['#내돈내듣','ON STAGE','VIBE 오디오'],
+      CSLink : ['서비스 소개','VIBE 멤버십'],
+    }
+  },
   components: {
-    HelloWorld
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
